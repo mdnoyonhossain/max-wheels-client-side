52cb5b57fe1069ebce47c154d573de9ba1ca5d65
MD Shahinur Rahman Nayon <mdshahinurrahmannayon@gmail.com>
2022-11-25 10:10:06 +0600

    all Seller Data Load and Delete Done

--- src/Layouts/DashboardLayout/DashboardLayout.js
+++ src/Layouts/DashboardLayout/DashboardLayout.js
@@ -22,7 +22,7 @@ const DashboardLayout = () => {
                 </div>
                 <div className="drawer-side">
                     <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-60 bg-base-100 text-base-content">
+                    <ul className="menu p-4 w-60 text-base-content">
 
                         <li><Link to="/dashboard">My Order</Link></li>
                         {
--- src/Pages/Dashboard/Dashboard/AllSeller/AllSeller.js
+++ src/Pages/Dashboard/Dashboard/AllSeller/AllSeller.js
@@ -1,28 +1,82 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useState } from 'react';
+import toast from 'react-hot-toast';
+import Loading from '../../../Shared/Loading/Loading';
 
-const AllSeller = () => {    
+const AllSeller = () => {
+    const [deleteingSeller, setDeletingSeller] = useState(null)
+    const { data: allSeller = [], isLoading, refetch } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/users/?email=seller')
+            const data = await res.json();
+            return data;
+        }
+    });
+
+
+    const handleSellerDelete = seller => {
+        console.log(seller._id);
+        fetch(`http://localhost:5000/users/${seller._id}`, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.deletedCount > 0){
+                toast.success('Seller Deleting Successfully');
+                refetch()
+            }
+        })
+    }
+    
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
 
     return (
         <div className="overflow-x-auto">
+            <h3 className="mb-4 font-bold text-3xl">All Seller</h3>
             <table className="table w-full">
                 <thead>
                     <tr>
                         <th></th>
                         <th>Name</th>
-                        <th>Job</th>
-                        <th>Favorite Color</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    
-                    <tr className="hover">
-                        <th>1</th>
-                        <td>name</td>
-                        <td>Desktop Support Technician</td>
-                        <td>Purple</td>
-                    </tr>
+                    {
+                        allSeller.map((seller, i) => <tr key={seller._id} className="hover">
+                            <th>{i + 1}</th>
+                            <td>{seller.name}</td>
+                            <td>{seller.email}</td>
+                            <td>{seller.role}</td>
+                            <td><label onClick={() => setDeletingSeller(seller)} htmlFor="seller-delete" className="btn btn-xs">Delete</label></td>
+                            
+                        </tr>)
+                    }
+
                 </tbody>
             </table>
+            {
+                deleteingSeller && <>
+                <input type="checkbox" id="seller-delete" className="modal-toggle" />
+            <div className="modal">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">{deleteingSeller.name}</h3>
+                    <p className="py-4"><b>Seller Email:</b> {deleteingSeller.email}</p>
+                    <p className=" text-2xl text-orange-500">Are you Sure Delete Seller</p>
+                    <div className="modal-action">
+                        <label onClick={() => handleSellerDelete(deleteingSeller)} htmlFor="seller-delete" className="btn bg-orange-500">Delete</label>
+                        <label htmlFor="seller-delete" className="btn bg-red-500">Cancel</label>
+                    </div>
+                </div>
+            </div>
+                </>
+            }
         </div>
 
     );
